144befdd39de475d565571b097bb01b98a98bedc
mekanics <alexandre.joly@mekanics.ch>
2014-06-05 21:36:42 +0200

    latest stable ruby version is 2.1.2

--- install.sh
+++ install.sh
@@ -27,12 +27,12 @@ cd -1
 # ruby
 echo "updating ruby"
 rbenv install 2.0.0-p451
-rbenv install 2.1.1
+rbenv install 2.1.2
 rbenv rehash
 # Try out a ruby.
-rbenv shell 2.1.1
+rbenv shell 2.1.2
 # Enable a newly installed ruby, persistently, globally.
-rbenv global 2.1.1
+rbenv global 2.1.2
 rbenv rehash
 
 gem install bundler
