26485a0939123fb2e210c3ab926f5a3057ca686e
Alexandre Joly <alexandre.joly@kilokilo.ch>
2020-12-14 09:56:35 +0100

    Configure bzip2 & zlib

--- python/config.zsh
+++ python/config.zsh
@@ -1,3 +1,14 @@
+# zlib
+export LDFLAGS="${LDFLAGS} -L/usr/local/opt/zlib/lib"
+export CPPFLAGS="${CPPFLAGS} -I/usr/local/opt/zlib/include"
+export PKG_CONFIG_PATH="/usr/local/opt/zlib/lib/pkgconfig"
+
+# bzip2
+export PATH="/usr/local/opt/bzip2/bin:$PATH"
+export LDFLAGS="${LDFLAGS} -L/usr/local/opt/bzip2/lib"
+export CPPFLAGS="${CPPFLAGS} -I/usr/local/opt/bzip2/include"
+
+# pyenv
 if command -v pyenv 1>/dev/null 2>&1; then
     eval "$(pyenv init -)"
     eval "$(pyenv virtualenv-init -)"
