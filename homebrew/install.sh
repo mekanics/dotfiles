a47b898abc7ed41b568cf74010e1e213f097aabd
Alexandre Joly <alexandre.joly@kilokilo.ch>
2021-11-03 16:08:35 +0100

    Use brew after install

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -4,5 +4,5 @@
 
 if test ! $(command -v brew); then
     curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash
-    source ~/.zshrc
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
