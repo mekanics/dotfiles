63e7cd1ab76ca696fadc093082afbf73356dc2ee
Alexandre Joly <alexandre.joly@kilokilo.ch>
2021-11-03 17:06:19 +0100

    update the nvm install and install the lts version of node

--- node/install.sh
+++ node/install.sh
@@ -3,14 +3,14 @@
 ## NVM
 
 if test ! $(command -v nvm); then
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.0/install.sh | bash
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 fi
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
-nvm install v14.15
+nvm install --lts
 
 ## YARN
 brew install yarn
