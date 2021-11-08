37f97129f6fda707c71640362f8fc8a40d04549c
Alexandre Joly <alexandre.joly@kilokilo.ch>
2021-11-08 11:03:14 +0100

    Setup perl for gnucash

---
+++ gnucash/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo cpan upgrade Test2
+sudo cpan -T B::Keywords
+sudo cpan install Finance::Quote
---
+++ perl/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+brew install perl
+
+eval "$(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib=$HOME/perl5)"
