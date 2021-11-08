824dbd982624cef9387cfdef030f4a149b1e5705
Alexandre Joly <alexandre.joly@kilokilo.ch>
2021-11-08 10:00:38 +0100

    Update the ruby setup

--- ruby/config.zsh
+++ ruby/config.zsh
@@ -1,3 +1,2 @@
-source /usr/local/share/chruby/chruby.sh
-source /usr/local/share/chruby/auto.sh
-chruby ruby-2.7.4
+source /opt/homebrew/opt/chruby/share/chruby/chruby.sh
+source /opt/homebrew/opt/chruby/share/chruby/auto.sh
---
+++ ruby/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## chruby
+
+brew install chruby
+brew install ruby-install
+
+ruby-install ruby
