2feec6d77c86708c09df66b7c2b0c2f93daeb5b5
Alexandre Joly <alexandre.joly@kilokilo.ch>
2021-11-08 11:12:48 +0100

    Update the perl setup

--- perl/install.sh
+++ perl/install.sh
@@ -2,4 +2,5 @@
 
 brew install perl
 
-eval "$(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib=$HOME/perl5)"
+eval "$(perl -I$HOME/.perl5/lib/perl5 -Mlocal::lib=$HOME/.perl5)"
+PERL_MM_OPT="INSTALL_BASE=$HOME/.perl5" cpan local::lib
