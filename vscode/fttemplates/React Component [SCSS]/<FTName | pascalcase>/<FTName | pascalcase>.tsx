a18ce72b2c9295c9412119ff04f071ec6e954aec
Alexandre Joly <alexandre.joly@kilokilo.ch>
2022-04-01 11:20:54 +0200

    Create a template for react components

---
+++ vscode/fttemplates/React Component [SCSS]/<FTName | pascalcase>/<FTName | pascalcase>.tsx
@@ -0,0 +1,15 @@
+import type { FC } from 'react'
+import clsx from 'clsx'
+import s from './<FTName | pascalcase>.module.scss'
+
+export type <FTName | pascalcase>Type = {}
+
+const <FTName | pascalcase>: FC<<FTName | pascalcase>Type> = () => {
+	return (
+		<div className={s.root}>
+			
+		</div>
+	)
+}
+
+export default <FTName | pascalcase>
