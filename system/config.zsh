6f0dda305d2018b7b865a08a030bdd4a7aca64b5
Alexandre Joly <alexandre.joly@mekanics.ch>
2023-05-22 10:27:15 +0200

    Disable the fastlane telemetry

--- system/config.zsh
+++ system/config.zsh
@@ -1,9 +1,3 @@
-export NVS_HOME="$HOME/.nvs"
-[ -s "$NVS_HOME/nvs.sh" ] && . "$NVS_HOME/nvs.sh"
-
-nvs auto on
-nvs use
-
 # Disable telemetry
 export NEXT_TELEMETRY_DISABLED=1     # next
 export STORYBOOK_DISABLE_TELEMETRY=1 # storybook
